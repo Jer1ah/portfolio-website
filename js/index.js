3c4928849371ab787597cfea65746b1b21a90f74
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-11-19 10:24:47 -0500

    finishing contact and footer styling

--- js/index.js
+++ js/index.js
@@ -12,3 +12,17 @@ const portfolioListItemController = (function(){
         });
     }
 }());
+
+
+const emailButtonController = (function(){
+    const _emailButton = document.querySelector('.contact__email');
+    const _emailIcon = document.querySelector('.contact__email img');
+
+    _emailButton.addEventListener('mouseenter', () => {
+        _emailIcon.src = 'images/email-blue.svg';
+    });
+
+    _emailButton.addEventListener('mouseleave', () => {
+        _emailIcon.src = 'images/email-white.svg';
+    });
+}());
